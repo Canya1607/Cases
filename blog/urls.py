143982acd1f74c25c982c655ced9de7ef31887d6
Canya1607 <hnenniy1610@gmail.com>
2018-03-12 14:03:16 +0200

    Added roots for login

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.conf.urls import include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+	url(r'^$', views.home, name='home'),
+    #url(r'^$', views.post_list, name='post_list'),
+    url(r'^login/$', views.login, name='login'),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
